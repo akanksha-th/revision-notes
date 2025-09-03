efe28f183d29c2dc693af670869782084e1ec074
akanksha-th <ak.t.kree@gmail.com>
2025-09-03 12:46:11 +0530

    updated chatbot

--- chatbot/local_llama_app.py
+++ chatbot/local_llama_app.py
@@ -31,4 +31,4 @@ output_parser = StrOutputParser
 chain = prompt|llm|output_parser
 
 if input_text:
-    st.write(chain.invoke({"question": input_text}))
+    st.write(chain.invoke({"question":input_text}))
